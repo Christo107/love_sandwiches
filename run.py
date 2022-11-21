dc85069469674f23489f53ca6fbb8895fc868ce0
Christo107 <christopher.odwyer82@gmail.com>
2022-11-21 19:08:23 +0000

    Tidy code to matchinstruction video

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ def validate_data(values):
 
 def update_sales_worksheet(data):
     """
-    Update sales worksheet, add new row with the list data provided.
+    Update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
@@ -68,9 +68,9 @@ def update_sales_worksheet(data):
 
 def update_surplus_worksheet(data):
     """
-    Update surplus worksheet, add new row with the list data provided.
+    Update surplus worksheet, add new row with the list data provided
     """
-    print("Updating sales worksheet...\n")
+    print("Updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
@@ -104,7 +104,6 @@ def main():
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     update_surplus_worksheet(new_surplus_data)
-    print(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
